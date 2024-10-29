f33d204f2359e81df6434f04cc5f53da92850642
Kao Kittisak <kittisakphiri@gmail.com>
2024-10-29 13:57:42 +0700

    a lot changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 export default {
     plugins: {
+        "postcss-import": {},
+        "tailwindcss/nesting": {},
         tailwindcss: {},
+        "postcss-custom-properties": {},
         autoprefixer: {},
     },
 };
